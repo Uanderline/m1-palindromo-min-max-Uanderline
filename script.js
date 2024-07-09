9916d7e824162fdd60eeeb5a20c9604dd2a8f155
Uanderline <45636326+Uanderline@users.noreply.github.com>
2024-07-08 22:32:49 -0300

    Desenvolvimento das functions isPalindrome e arrayMaxMin

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function isPalindrome(str){
     /* Seu código aqui */
+    const textoLower = str.toLowerCase().replace(/[\W_]/g, '')
+    const textoReverso = textoLower.split('').reverse().join('');
+    return textoLower === textoReverso;
 }
 
 function arrayMaxMin(arr){
     /* Seu código aqui */
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+
+    return [min, max]
 }
